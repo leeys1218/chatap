9f37a2bbec794cc45c603d70cce631149cdcc373
YeonSang <leeys12181811@gmail.com>
2023-06-24 21:08:44 +0900

    modify backend cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ def create_app():
 
     # middleware def
     origins = [
-        "http://localhost:5173",
+        "http://127.0.0.1:5173",
         "0.0.0.0:5173",
     ]
     app.add_middleware(
